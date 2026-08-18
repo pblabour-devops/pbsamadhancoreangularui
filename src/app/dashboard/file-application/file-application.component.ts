f775aa85912ac0d289bae51f72e72d6e9a5124c7
Amit Kumar <amitkumar@example.com>
2026-08-18 16:08:02 +0530

    made first page dynamic

--- src/app/dashboard/file-application/file-application.component.ts
+++ src/app/dashboard/file-application/file-application.component.ts
@@ -14,11 +14,18 @@ import { CommonOpsService } from 'src/app/shared/common-ops-service';
   standalone: false
 })
 export class FileApplicationComponent {
-  selectedIssues!: any
-
+  selectedIssues: any[] = []
+  categories: any[] = [];
  filingTypes: any[]
-
-
+  public ComplainantType ={
+    SELF : 1,
+    NOMINEE : 2,
+    LEGAL_HEIR : 3,
+    LEGAL_REPRESENTATIVE : 4,
+    AUTHORIZED_REPRESENTATIVE : 5,
+    GROUP_OF_WORKERS : 6,
+}
+  
   selectedFilingType = 1;
 
   protected ngUnsubscribe: Subject<void> = new Subject<void>();
@@ -31,13 +38,80 @@ export class FileApplicationComponent {
 
     
   ngAfterViewInit() {
-    this.appHttpRequestHandlerService.httpGet({}, "Complaints", "getSelfComplaints").pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((data: GenericFormModel<IComplainantTypeComplaintTypeMapping>) => { 
-        this.filingTypes = data.enumTemplateLists.find(e => e.selectListTypeCode == 'ComplainantTypeEnum').selectListItems
-        console.log('data', data);
-      });
+    this.getComplainantComplaints(this.ComplainantType.SELF);
   }
 
+ getComplainantComplaints(complainantType: number) {
+  this.appHttpRequestHandlerService
+    .httpGet(
+      { complainantType: complainantType },
+      "Complaints",
+      "getComplainantComplaints"
+    )
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe((data: GenericFormModel<any>) => {
+
+      this.filingTypes =
+        data.enumTemplateLists.find(
+          e => e.selectListTypeCode == 'ComplainantTypeEnum'
+        ).selectListItems;
+
+      console.log('data', data);
+
+      const categoryNames: { [key: number]: string } = {
+        1: 'Payment Related',
+        2: 'Job(Service Condition) Related',
+        3: 'File Appeal',
+        4: 'Penalty/Composition'
+      };
+
+      const groupedCategories: any[] = [];
+
+      Object.keys(categoryNames).forEach(key => {
+
+        const type = +key;
+
+        const issues = data.formModel
+          .filter(x => x.complaintCategoryType === type)
+          .map(x => ({
+            id: x.id,
+            label: x.complaintTitle,
+            hasInfo: x.hasInfo,
+            info: x.info,
+            disabled: 0,
+            complaintCategoryType: x.complaintCategoryType
+          }));
+
+        if (issues.length > 0) {
+          groupedCategories.push({
+            title: categoryNames[type],
+            issues: issues
+          });
+        }
+
+      });
+
+      this.categories = groupedCategories;
+
+      console.log('grouped categories', this.categories);
+    });
+}
+
+  toggleComplaint(complanant){
+    this.getComplainantComplaints(complanant.value);
+    console.log('complanant', complanant);
+  }
+
+  toggleIssue(issue: any): void {
+    console.log('issue', issue);
+    const index = this.selectedIssues.indexOf(issue.id);
+
+    if (index > -1) {
+      this.selectedIssues.splice(index, 1);
+    } else {
+      this.selectedIssues.push(issue);
+    }
+  }
   selectedissue(){
     if(this.selectedIssues.length === 0) {
       alert('Please select at least one issue before proceeding.');
